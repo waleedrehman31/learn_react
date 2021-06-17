4ab06d601efe69b0bc7f75d2e2da56556fbc2563
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-06-17 21:21:58 +0500

    Filtering Data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Additem from "./AddItem";
 import ItemsDisplay from "./ItemsDisplay";
 
 function App() {
-  // eslint-disable-next-line
   const [filters, setFilters] = useState({});
   const [data, setData] = useState({ items: [] });
 
@@ -22,10 +21,35 @@ function App() {
     console.log(data);
   };
 
+  const filterData = (data) => {
+    const filteredData = [];
+    if (!filters.name) {
+      return data;
+    }
+    for (const item of data) {
+      if (filters.name !== "" && item.name !== filters.name) {
+        continue;
+      }
+      if (filters.price !== 0 && item.price > filters.price) {
+        continue;
+      }
+      if (filters.type !== "" && item.type !== filters.type) {
+        continue;
+      }
+      if (filters.brand !== "" && item.brand !== filters.brand) {
+        continue;
+      }
+
+      filteredData.push(item);
+    }
+
+    return filteredData;
+  };
+
   return (
     <div className="container">
       <div className="row mt-3">
-        <ItemsDisplay items={data["items"]} />
+        <ItemsDisplay items={filterData(data["items"])} />
       </div>
       <div className="row mt-3">
         <SearchBar updateSearchParams={updateFilters} />
